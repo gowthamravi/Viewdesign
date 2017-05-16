ea6bb6659213ff99e83cf483496b564d9db250ad
Gowtham <gowthamravikumar@doodleblue011.local>
2017-05-16 19:11:20 +0530

    commit

--- Example/ViewDesign/ViewController.swift
+++ Example/ViewDesign/ViewController.swift
@@ -7,6 +7,8 @@
 //
 
 import UIKit
+import ViewDesign
+
 
 class ViewController: UIViewController {
 
--- ViewDesign/Classes/ViewDesign.swift
+++ ViewDesign/Classes/ViewDesign.swift
@@ -9,7 +9,8 @@
 import Foundation
 
 
-public class ViewDesign : UILabel {
+
+extension UIView{
     @IBInspectable var cornerRadius: CGFloat {
         get {
             return layer.cornerRadius
@@ -19,7 +20,6 @@ public class ViewDesign : UILabel {
             layer.masksToBounds = newValue > 0
         }
     }
-    
     @IBInspectable var borderwidth: CGFloat {
         get {
             return layer.borderWidth
@@ -29,7 +29,6 @@ public class ViewDesign : UILabel {
             layer.masksToBounds = newValue > 0
         }
     }
-    
     @IBInspectable var bordercolor: UIColor {
         get {
             return self.bordercolor
@@ -38,28 +37,5 @@ public class ViewDesign : UILabel {
             layer.borderColor = newValue.cgColor
         }
     }
-    
-//    public func startBlinking() {
-//        
-//        let option: UIViewAnimationOptions = .curveEaseIn
-//        UIView.animate(withDuration: 0.25, delay: 0.0, options: option, animations: {  () -> Void in
-//            self.alpha = 0
-//        }, completion: nil)
-//    }
-//    
-//    /**
-//     Tells the label to stop blinking.
-//     */
-//    public func stopBlinking() {
-//        alpha = 1
-//        layer.removeAllAnimations()
-//    }
-    
-//   public func shake() {
-//        let animation = CAKeyframeAnimation(keyPath: "transform.translation.x")
-//        animation.timingFunction = CAMediaTimingFunction(name: kCAMediaTimingFunctionLinear)
-//        animation.duration = 0.4
-//        animation.values = [-20.0, 20.0, -20.0, 20.0, -10.0, 10.0, -5.0, 5.0, 0.0 ]
-//        self.layer.add(animation, forKey: "shake")
-//    }
 }
+
